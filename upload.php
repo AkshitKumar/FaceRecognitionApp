1364e7d5fe0976b23e6c98cbc4ff0bf641236c26
AkshitKumar <akshitkumar100@gmail.com>
2015-08-24 15:17:19 +0530

    Minor changes

--- upload.php
+++ upload.php
@@ -7,6 +7,6 @@ $img = str_replace('data:image/png;base64,','', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
 $file = UPLOAD_DIR . $name . '.png';
-$success = file_put_contents($file, $data);
+$success = file_put_contents($file, $data)
 
 ?>
